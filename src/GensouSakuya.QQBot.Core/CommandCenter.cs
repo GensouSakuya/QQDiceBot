5770e32d37ba3a70116b51789dfe6a57ae8a5e18
GensouSakuya <22719048+GensouSakuya@users.noreply.github.com>
2021-11-25 17:19:39 +0800

    fix

--- src/GensouSakuya.QQBot.Core/CommandCenter.cs
+++ src/GensouSakuya.QQBot.Core/CommandCenter.cs
@@ -48,7 +48,7 @@ namespace GensouSakuya.QQBot.Core
             if(qqNo.HasValue && groupNo.HasValue)
             {
                 member = await GroupMemberManager.Get(qqNo.Value, groupNo.Value);
-                if(DataManager.Instance.GroupIgnore.ContainsKey((member.GroupNumber, member.QQ)))
+                if (member != null && (DataManager.Instance?.GroupIgnore?.ContainsKey((member.GroupNumber, member.QQ)) ?? false))
                 {
                     return;
                 }
