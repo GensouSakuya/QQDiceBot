f58b537916ff7f9230f335fedff958ee6aef88bc
GensouSakuya <22719048+GensouSakuya@users.noreply.github.com>
2024-12-31 16:37:51 +0800

    upgrade .admin

---
+++ src/GensouSakuya.QQBot.Core/Commands/Obsolete/NullManager.cs
@@ -0,0 +1,20 @@
+﻿//using System.Collections.Generic;
+//using System.Threading.Tasks;
+//using GensouSakuya.QQBot.Core.Base;
+//using GensouSakuya.QQBot.Core.Model;
+//using GensouSakuya.QQBot.Core.PlatformModel;
+//using net.gensousakuya.dice;
+
+//namespace GensouSakuya.QQBot.Core.Commands
+//{
+//    [Command("null")]
+//    public class NullHandler : BaseManager
+//    {
+//        public override async Task ExecuteAsync(MessageSource source, List<string> command, List<BaseMessage> originMessage, UserInfo qq, Group group, GroupMember member, GuildUserInfo guildUser, GuildMember guildmember)
+//        {
+//            await Task.Yield();
+//            MessageManager.SendToSource(source, "略略略😝");
+//            return;
+//        }
+//    }
+//}
---
+++ src/GensouSakuya.QQBot.Core/Handlers/AdminHandler.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GensouSakuya.QQBot.Core.Base;
+using GensouSakuya.QQBot.Core.Interfaces;
+using GensouSakuya.QQBot.Core.Model;
+using GensouSakuya.QQBot.Core.PlatformModel;
+
+namespace GensouSakuya.QQBot.Core.Commands
+{
+    internal class AdminHandler : IMessageCommandHandler
+    {
+        public async Task ExecuteAsync(MessageSource source, IEnumerable<string> command, List<BaseMessage> originMessage, SourceFullInfo sourceInfo)
+        {
+            await Task.Yield();
+            if (source.Type != MessageSourceType.Private && source.Type != MessageSourceType.Friend)
+                return;
+            if (sourceInfo.QQ == null || sourceInfo.QQ.QQ != DataManager.Instance.AdminQQ)
+                return;
+            var firstCommand = command.FirstOrDefault();
+            if (firstCommand == null)
+                return;
+            switch (firstCommand.ToLower())
+            {
+                case "say":
+                {
+                    command = command.Skip(1);
+                    if (command.Count() < 2)
+                        return;
+                    if (!long.TryParse(command.First(), out var groupNumber))
+                    {
+                        return;
+                    }
+
+                    command = command.Skip(1);
+                    var message = string.Join(" ", command);
+                    MessageManager.SendTextMessageToGroup(groupNumber, message);
+                    return;
+                }
+                case "rename":
+                    command = command.Skip(1);
+                    if (command.Count() < 1)
+                        return;
+                    var name = command.ElementAt(0);
+                    DataManager.Instance.BotName = name;
+                    MessageManager.SendTextMessage(MessageSourceType.Friend, "改名成功", DataManager.Instance.AdminQQ);
+                    return;
+                case "save":
+                    DataManager.Save();
+                    MessageManager.SendTextMessage(MessageSourceType.Friend, "保存成功", DataManager.Instance.AdminQQ);
+                    return;
+            }
+        }
+    }
+}
