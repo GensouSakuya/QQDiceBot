b55149039e6d69377885a833d4d285cec789fb6c
GensouSakuya <22719048+GensouSakuya@users.noreply.github.com>
2025-02-28 16:58:17 +0800

    fix regex bug

--- src/GensouSakuya.QQBot.Core/Helpers/WeiboHelper.cs
+++ src/GensouSakuya.QQBot.Core/Helpers/WeiboHelper.cs
@@ -8,11 +8,11 @@ namespace GensouSakuya.QQBot.Core.Helpers
         //正则部分参考rsshub：https://github.com/DIYgod/RSSHub/blob/master/lib/routes/weibo/utils.ts
         static Regex _faceRegex = new Regex("<span class=[\"']url-icon[\"']><img\\s[^>]*?alt=[\"']?([^>]+?)[\"']?\\s[^>]*?\\/?><\\/span>");
         static Regex _newLineRegex = new Regex("<br\\s/>");
-        static Regex _tagRegex = new Regex("<a\\s+href=\".*search.*\"\\s+data-hide=\"\"><span\\s+class=\"surl-text\">(.*?)<\\/span><\\/a>");
-        static Regex _videoRegex = new Regex("<a\\s+href=\"(.*video.weibo.com.*)\"\\s+data-hide=\"\"><span\\s+class='url-icon'><img.*_video_.*></span><span\\s+class=\"surl-text\">(.*)</span></a>");
+        static Regex _tagRegex = new Regex("<a\\s+?href=\".*?search.*?\"\\s+?data-hide=\"\"><span\\s+class=\"surl-text\">(.*?)<\\/span><\\/a>");
+        static Regex _videoRegex = new Regex("<a\\s+href=\"(.*?video.weibo.com.*?)\"\\s+data-hide=\"\"><span\\s+class='url-icon'><img.*?_video_.*?></span><span\\s+class=\"surl-text\">(.*?)</span></a>");
         static Regex _fullTextRegex = new Regex("<a href=\"(.*?)\">全文<\\/a>");
-        static Regex _repostRegex = new Regex("<a href='\\/n\\/.*'>(.*?)<\\/a>");
-        static Regex _urlRegex = new Regex("<a\\s+href=\"(.*?)\"\\s+.*class=\"surl-text\">网页链接</span></a>");
+        static Regex _repostRegex = new Regex("<a href='\\/n\\/.*?'>(.*?)<\\/a>");
+        static Regex _urlRegex = new Regex("<a\\s+href=\"(.*?)\"\\s+?.*?class=\"surl-text\">网页链接</span></a>");
 
         public static string FilterHtml(string originText)
         {
